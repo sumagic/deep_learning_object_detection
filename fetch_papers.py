0eb6f65101e4d26bcd71f7eadf366635cc298552
kangxu <999999999999>
2022-04-21 00:50:01 +0800

    fix fetch requests

--- fetch_papers.py
+++ fetch_papers.py
@@ -33,7 +33,14 @@ with codecs.open(filename, encoding='utf-8', mode='r', buffering=1, errors='stri
                 if(not os.path.exists(os.path.join(section_path, paper + '.pdf'))):
                     print('Fetching', paper)
                     try:
-                        response = requests.get(url)
+                        print("url: ", url)
+                        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+                        headers = {'User-Agent' : user_agent,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8','Connection': 'keep-alive'}  #here is the most importent
+                        response = requests.get(url, headers=headers)
                         with open(os.path.join(section_path, paper + '.pdf'), 'wb') as f:
                             f.write(response.content)
                     except requests.exceptions.RequestException as e:
